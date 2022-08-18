da753441876cfbaffd620c6d6a697a6920213750
jimersylee <jimersylee@gmail.com>
2022-08-18 09:36:19 +0800

    feat(add wechat): add wechat public

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -10,7 +10,7 @@ export const Footer = (props) => {
         <DarkModeButton className='text-center pt-4'/>
 
         <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${startYear}${currentYear}`} {BLOG.AUTHOR}. All rights reserved.</div>
+            <div className='text-center'> &copy;{`${startYear}${currentYear}`} {BLOG.AUTHOR}. All rights reserved. 公众号: 技术管理方法论</div>
             <div className="md:p-0 text-center md:text-right text-xs">
                 {/* 右侧链接 */}
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
